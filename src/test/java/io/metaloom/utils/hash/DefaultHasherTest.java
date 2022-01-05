71343b25665803a976b6e6ac98809ed4de4304d1
Johannes Schüth <git@jotschi.de>
2022-01-05 16:02:44 +0100

    Add missing shim

--- src/test/java/io/metaloom/utils/hash/DefaultHasherTest.java
+++ src/test/java/io/metaloom/utils/hash/DefaultHasherTest.java
@@ -3,9 +3,20 @@ package io.metaloom.utils.hash;
 import static org.junit.Assert.assertEquals;
 
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.junit.Test;
 
+import io.metaloom.utils.hash.impl.MemorySegmentHasher;
+
+/**
+ * Run Test with
+ * 
+ * <pre>
+ *  --add-modules jdk.incubator.foreign --add-exports=java.base/sun.nio.ch=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-exports=jdk.unsupported/sun.misc=ALL-UNNAMED
+ * </pre>
+ *
+ */
 public class DefaultHasherTest extends AbstractHasherTest {
 
 	@Test
@@ -16,7 +27,14 @@ public class DefaultHasherTest extends AbstractHasherTest {
 		assertEquals(LARGE_SHA512, h1);
 
 		String h2 = HashUtils.computeSHA512(file);
-		assertEquals(h1,h2);
+		assertEquals(h1, h2);
+	}
+
+	@Test
+	public void testHugeFile() {
+		String file = "/dev/zero";
+		String hash = new MemorySegmentHasher().computeSHA512(Paths.get(file));
+		System.out.println("Hash: " + hash);
 	}
 
 }
